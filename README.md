950cfe323d2c1d4b8c6d6a4b342b253c2282a569
leopardpan <panbaixin123@163.com>
2016-07-07 11:04:00 +0800

    切换jekyll Vno主题

---
+++ README.md
@@ -0,0 +1,13 @@
+# Vno - Jekyll
+
+[Vno Jekyll](https://github.com/onevcat/vno-jekyll) is a theme for [Jekyll](http://jekyllrb.com). It is a port of my Ghost theme [vno](https://github.com/onevcat/vno), which is originally developed from [Dale Anthony's Uno](https://github.com/daleanthony/uno).
+
+## Live Demo
+
+See [Vno - Jekyll](http://vno.onevcat.com) site and [my blog](http://onevcat.com).
+
+You can also find some instruction as well as other sites using Vno theme in [this page](http://vno.onevcat.com/2016/02/hello-world-vno/).
+
+## Licence
+
+Great thanks to [Dale Anthony](https://github.com/daleanthony) and his [Uno](https://github.com/daleanthony/uno). Vno Jekyll is based on Uno, and contains a lot of modification on page layout, animation, font and some more things I can not remember. Vno Jekyll is followed with Uno and be licensed as [Creative Commons Attribution 4.0 International](http://creativecommons.org/licenses/by/4.0/). See the link for more information.
--- js/main.js
+++ js/main.js
@@ -1,35 +1,33 @@
 $(document).ready(function() {
 
-  $('body').removeClass('no-js');
-
   $('a.blog-button').click(function() {
+    // If already in blog, return early without animate overlay panel again.
+    if (location.hash && location.hash == "#blog") return;
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
+    $('.main-post-list').removeClass('hidden');
     currentWidth = $('.panel-cover').width();
     if (currentWidth < 960) {
       $('.panel-cover').addClass('panel-cover--collapsed');
-      $('.content-wrapper').addClass('animated slideInRight');
     } else {
       $('.panel-cover').css('max-width',currentWidth);
-      $('.panel-cover').animate({'max-width': '465px', 'width': '26%'}, 400, swing = 'swing', function() {} );
+      $('.panel-cover').animate({'max-width': '700px', 'width': '30%'}, 400, swing = 'swing', function() {} );
     }
   });
 
   if (window.location.hash && window.location.hash == "#blog") {
     $('.panel-cover').addClass('panel-cover--collapsed');
+    $('.main-post-list').removeClass('hidden');
   }
 
   if (window.location.pathname.substring(0, 5) == "/tag/") {
     $('.panel-cover').addClass('panel-cover--collapsed');
   }
-  
-  if (window.location.pathname.substring(0, 6) == "/page/") {
-    $('.panel-cover').addClass('panel-cover--collapsed');
-  }
 
   $('.btn-mobile-menu__icon').click(function() {
     if ($('.navigation-wrapper').css('display') == "block") {
-      $('.navigation-wrapper').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+      $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
         $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
+        $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
       });
       $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
 
@@ -41,32 +39,14 @@ $(document).ready(function() {
 
   $('.navigation-wrapper .blog-button').click(function() {
     if ($('.navigation-wrapper').css('display') == "block") {
-      $('.navigation-wrapper').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+      $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
         $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
+        $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
       });
 
       $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
     }
+    
     $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
   });
-
-  $("article.post-container--single a[href^=http]").attr("target", "_blank");
-  $("article.post-container--single a[href^=mailto]").attr("target", "_blank");
-  
-  
-  
-  $(function(){
-		
-		$('.img-click').click(function(){
-            document.getElementById("img-content").src=$(this).attr("data");
-			$('.img-background').fadeIn(200);
-			$('.img-border').fadeIn(400);
-            
-		});
-		$('.img-background').click(function(){
-			$('.img-background').fadeOut(200);
-			$('.img-border').fadeOut(200);
-		});
-        
-	});
 });
