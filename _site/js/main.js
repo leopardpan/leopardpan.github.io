3f9b0ef26107fe9334054e31e15abbc41bf15a28
leopardpan <panbaixin123@163.com>
2016-10-01 19:13:28 +0800

    fix gitignoore

---
+++ _site/js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+  $('a.blog-button').click(function() {
+    // If already in blog, return early without animate overlay panel again.
+    if (location.hash && location.hash == "#blog") return;
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
+    $('.main-post-list').removeClass('hidden');
+    currentWidth = $('.panel-cover').width();
+    if (currentWidth < 960) {
+      $('.panel-cover').addClass('panel-cover--collapsed');
+    } else {
+      $('.panel-cover').css('max-width',currentWidth);
+      $('.panel-cover').animate({'max-width': '700px', 'width': '30%'}, 400, swing = 'swing', function() {} );
+    }
+  });
+
+  if (window.location.hash && window.location.hash == "#blog") {
+    $('.panel-cover').addClass('panel-cover--collapsed');
+    $('.main-post-list').removeClass('hidden');
+  }
+
+  if (window.location.pathname.substring(0, 5) == "/tag/") {
+    $('.panel-cover').addClass('panel-cover--collapsed');
+  }
+
+  $('.btn-mobile-menu__icon').click(function() {
+    if ($('.navigation-wrapper').css('display') == "block") {
+      $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+        $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
+        $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+      });
+      $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
+
+    } else {
+      $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
+    }
+    $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
+  });
+
+  $('.navigation-wrapper .blog-button').click(function() {
+    if ($('.navigation-wrapper').css('display') == "block") {
+      $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+        $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
+        $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+      });
+
+      $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
+    }
+    
+    $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
+  });
+});
